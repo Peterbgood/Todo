b08e0516c210a63d1db849fe6960c4d0aa476378
Peterbgood <pwggithub@gmail.com>
2024-09-29 20:17:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function renderList() {
         listContainer.innerHTML = '';
         const list = lists[selectedListIndex];
         const listHtml = `
-            <div class="mt-3 input-group">
+            <div class="mt-1 input-group">
                 <input type="text" id="new-item-input" class="form-control fw-bold text-center" maxlength="30" placeholder="Enter new item">
                 <button class="btn btn-primary btn-sm" onclick="addItem(${selectedListIndex})">Add</button>
             </div>
