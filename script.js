789919404130aa222fa536f156f7bddaea4c5a59
Peterbgood <pwggithub@gmail.com>
2024-10-14 21:10:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Initialize elements
 const listSelector = document.getElementById('list-selector');
 const newListInput = document.getElementById('new-list-input');
@@ -43,17 +44,17 @@ function renderList() {
                 <input type="text" id="new-item-input" class="form-control" maxlength="30" placeholder="Enter new item">
                 <button class="btn btn-primary btn-sm" onclick="addItem(${selectedListIndex})">Add</button>
             </div>
-            <div id="list" class="mt-2">
+            <ul id="list" class="list-group mt-2">
                 ${list.items.map((item, itemIndex) => `
-                    <div class="d-flex align-items-center border-bottom pb-2 mb-2">
-                        ${item}
-                        <div class="ms-auto">
-                            <button class="btn btn-primary btn-sm me-1" onclick="moveUp(${selectedListIndex}, ${itemIndex})">Up</button>
-                            <button class="btn btn-danger btn-sm" onclick="deleteItem(${selectedListIndex}, ${itemIndex})">Delete</button>
-                        </div>
-                    </div>
+             <li class="list-group-item d-flex align-items-center">
+    ${item}
+    <div class="ms-auto">
+        <button class="btn btn-primary btn-sm me-1" onclick="moveUp(${selectedListIndex}, ${itemIndex})">Up</button>
+        <button class="btn btn-danger btn-sm" onclick="deleteItem(${selectedListIndex}, ${itemIndex})">Delete</button>
+    </div>
+</li>
                 `).join('')}
-            </div>
+            </ul>
             <button class="btn btn-danger w-100 mt-2" onclick="deleteList(${selectedListIndex})">Delete List</button>
         `;
         listContainer.insertAdjacentHTML('beforeend', listHtml);
@@ -66,7 +67,7 @@ function renderList() {
         });
     } else if (selectedListIndex === 'new') {
         const newListHtml = `
-            <div class="mt-1 input-group">
+            <div class="mt-1  input-group">
                 <input type="text" id="new-list-input" class="form-control" maxlength="30" placeholder="Enter new list title">
                 <button class="btn btn-primary btn-sm" id="save-new-list-btn">Save</button>
             </div>
@@ -121,8 +122,8 @@ function addItem(listIndex) {
     if (newItem) {
         lists[listIndex].items.push(newItem);
         localStorage.setItem('lists', JSON.stringify(lists));
+        newItemInput.value = '';
         renderList();
-        document.getElementById('new-item-input').focus(); // Get the element again after rendering
     }
 }
 
